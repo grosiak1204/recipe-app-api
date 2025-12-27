5c10bb245222d3139e078301c1d247eb89fcf05c
grosiak1204 <grosiak1204@gmail.com>
2025-12-27 16:47:12 +0100

    Confidure docker compose to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -20,7 +20,8 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
+            except (Psycopg2Error, OperationalError) as e:
+                print(e)
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
